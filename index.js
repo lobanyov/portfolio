57dacb4c16eb8500bc47c97221589cd470be3e73
lobanyov <stevenson@tut.by>
2019-10-24 23:15:54 +0300

    feat: add iframes

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-let isEnabled = true;
+let eduSection = document.querySelector('.edu-section');
+let eduList = document.querySelector('.edu-section__list');
+eduSection.addEventListener('click', showListEdu);
 
-document.querySelector('.edu-section').addEventListener('click', showListEdu);
+let delay = false;
 
 function showListEdu() {
-  if (document.querySelector('.edu-section__list').classList.contains('active')) {
+	if (delay) return;
+	delay = true;
+
+  if (eduList.classList.contains('active')) {
     setTimeout(() => {
-      document.querySelector('.edu-section__list').classList.remove('active');
+			eduList.classList.remove('active');
+			delay = false;
     }, 0);
-  } else {
+	} else {
     setTimeout(() => {
-      document.querySelector('.edu-section__list').classList.add('active');
-    }, 240);
-  }
+			eduList.classList.add('active');
+			delay = false;
+		}, 240);
+	}
   
   toggleBtn.classList.toggle('active');
-  document.querySelector('.edu-section').classList.toggle('active');
+  eduSection.classList.toggle('active');
 }
 
+let isEnabled = true;
 let slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -32,7 +40,7 @@ function hideSlide(direction) {
 	});
 }
 
-function showItem(direction) {
+function showSlide(direction) {
 	slides[currentSlide].classList.add('next-slide', direction);
 	slides[currentSlide].addEventListener('animationend', function() {
 		this.classList.remove('next-slide', direction);
@@ -44,13 +52,13 @@ function showItem(direction) {
 function nextSlide(index) {
 	hideSlide('to-left');
 	changeCurrentSlide(index + 1);
-	showItem('from-right');
+	showSlide('from-right');
 }
 
 function previousSlide(index) {
 	hideSlide('to-right');
 	changeCurrentSlide(index - 1);
-	showItem('from-left');
+	showSlide('from-left');
 }
 
 document.querySelector('.project-section__btn-left').addEventListener('click', function() {
@@ -77,7 +85,7 @@ const swipedetect = (element) => {
 
 	let threshold = 150;
 	let restraint = 100;
-	let allowedTime = 300;
+	let allowedTime = 500;
 
 	surface.addEventListener('mousedown', function(e) {
 		startX = e.pageX;
@@ -86,10 +94,22 @@ const swipedetect = (element) => {
 		e.preventDefault();
 	}, false);
 
+	let links = document.querySelectorAll('.slide__link');
+	links.forEach(link => {
+		link.onclick = () => false;
+	});
+
 	surface.addEventListener('mouseup', function(e) {
+		console.log(e.target.parentElement)
 		distX = e.pageX - startX;
 		distY = e.pageY - startY;
 		elapsedTime = new Date().getTime() - startTime;
+
+		if (elapsedTime < 150) {
+			e.target.parentElement.onclick = () => true;
+			e.target.parentElement.click();
+		}
+
 		if (elapsedTime <= allowedTime) {
 			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
 				if ((distX > 0)) {
@@ -108,16 +128,6 @@ const swipedetect = (element) => {
 
 	surface.addEventListener('touchstart', function(e) {
 
-			if (e.target.classList.contains('.project-section__btn-left')) {
-				if (isEnabled) {
-					previousSlide(currentSlide);
-				}
-			} else {
-				if (isEnabled) {
-					nextSlide(currentSlide);
-				}
-			}
-
 			var touchobj = e.changedTouches[0];
 			startX = touchobj.pageX;
 			startY = touchobj.pageY;
@@ -125,24 +135,30 @@ const swipedetect = (element) => {
 			e.preventDefault();
 	}, false);
 
-	surface.addEventListener('touchmove', function(e){
+	surface.addEventListener('touchmove', function(e) {
 			e.preventDefault();
 	}, false);
 
-	surface.addEventListener('touchend', function(e){
+	surface.addEventListener('touchend', function(e) {
 			var touchobj = e.changedTouches[0];
 			distX = touchobj.pageX - startX;
 			distY = touchobj.pageY - startY;
 			elapsedTime = new Date().getTime() - startTime;
-			if (elapsedTime <= allowedTime){
-					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+
+			if (elapsedTime < 200) {
+				if (e.target.classList.contains('slide-btn')) return;
+				e.target.click();
+			}
+
+			if (elapsedTime <= allowedTime && elapsedTime > 200) {
+					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
 							if ((distX > 0)) {
 								if (isEnabled) {
 									previousSlide(currentSlide);
 								}
 							} else {
 								if (isEnabled) {
-									nextItem(currentSlide);
+									nextSlide(currentSlide);
 								}
 							}
 					}
@@ -153,3 +169,50 @@ const swipedetect = (element) => {
 
 var element = document.querySelector('.slider-container');
 swipedetect(element);
+
+let toggleButtons = document.querySelectorAll('.slide-btn');
+
+function showDescription() {
+	toggleButtons.forEach(button => {
+		button.innerHTML = 'show<br> description';
+		button.style.background = '#e1ddec';
+	});
+}
+
+function hideDescription() {
+	toggleButtons.forEach(button => {
+		button.innerHTML = 'hide<br> description';
+		button.style.background = '#d2c927';
+	});
+}
+
+function changeDisplayDesc() {
+	let descriptions = document.querySelectorAll('.project-description');
+
+	for (let i = 0; i < descriptions.length; i++) {
+		descriptions[i].classList.toggle('active');
+	}
+	
+	if (document.querySelector('.project-description').classList.contains('active')) {
+		hideDescription();
+	} else {
+		showDescription();
+	}
+}
+
+toggleButtons.forEach(button => {
+	button.addEventListener('touchstart', changeDisplayDesc);
+	button.addEventListener('click', changeDisplayDesc);
+
+	button.onmouseover = function() {
+		button.style.background = '#d2c927';
+	}
+	
+	button.onmouseout = function() {
+		if (document.querySelector('.project-description').classList.contains('active')) {
+			button.style.background = '#d2c927';
+			return;
+		}
+		button.style.background = '#e1ddec';
+	}
+});
